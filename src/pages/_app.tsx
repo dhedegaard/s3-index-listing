e3f21c0f3fd9a4b3d691b95a2716b8f3c74023d8
Dennis Hedegaard <dennis@dhedegaard.dk>
2021-05-28 22:27:24 +0200

    Added some global styling.

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import { Head } from "next/document";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  }
+`;
+
+const theme = {};
+
+export default function App({ Component, pageProps }: any) {
+  return (
+    <>
+      <title>S3 Index</title>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
