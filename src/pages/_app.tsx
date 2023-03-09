95707f42b794198b1113a35a5d0efde24d72f3fb
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-03-09 11:08:46 +0000

    Moved global css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,5 @@
 import { createGlobalStyle, ThemeProvider } from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  }
-`;
+import '../styles/global.css'
 
 const theme = {};
 
@@ -15,7 +7,6 @@ export default function App({ Component, pageProps }: any) {
   return (
     <>
       <title>S3 Index</title>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
