1e76a3ae21df655df5a487991930bc54ce5c26d3
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-01-15 20:38:28 +0100

    jsdoc for env vars

--- src/server-env.ts
+++ src/server-env.ts
@@ -1,9 +1,19 @@
 import { z } from "zod";
 
 const serverEnvSchema = z.object({
+  /** @example `eu-west-1` */
   S3_REGION: z.string().min(1),
+  /** @example `s3-bucket-name */
   S3_BUCKET: z.string().min(1),
+  /**
+   * IAM access key, with permissions to the bucket.
+   *
+   * @example `AIKA...`
+   */
   ACCESS_KEY: z.string().min(1),
+  /**
+   * IAM secret access key, with permissions to the bucket.
+   */
   SECRET_ACCESS_KEY: z.string().min(1),
 });
 export const SERVER_ENV = serverEnvSchema.parse(
