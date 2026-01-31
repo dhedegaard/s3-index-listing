42e0b0191bc835369f0772554200a1478e7493e7
Dennis Hedegaard <dennis@dhedegaard.dk>
2026-01-31 23:42:12 +0100

    eslint prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from 'eslint-config-prettier/flat'
 
 export default defineConfig([
     ...nextVitals,
     ...nextTs,
+    prettier,
     // Override default ignores of eslint-config-next.
     globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
