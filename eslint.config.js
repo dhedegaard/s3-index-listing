a65d81cd8c66f7df59a34c1009eb938895cb27a1
Dennis Hedegaard <dennis@dhedegaard.dk>
2026-01-31 22:48:00 +0100

    migrated next lint -> eslint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+    ...compat.extends("next/core-web-vitals", "plugin:tailwindcss/recommended"),
+    {
+        ignores: [".next/**", "postcss.config.mjs"],
+    },
+];
+
+export default eslintConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,9 @@
-export default {
+const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
+
+export default config;
+
